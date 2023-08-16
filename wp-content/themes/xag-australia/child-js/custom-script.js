ff1b997d2b0e25b130dbfe24b0d97ff29c30393e
XAG-Australia <david@thesydney.agency>
2023-08-16 13:41:48 +1000

    Update

--- wp-content/themes/xag-australia/child-js/custom-script.js
+++ wp-content/themes/xag-australia/child-js/custom-script.js
@@ -1,10 +1,1 @@
-/**
-* Set the URL to redirect to on login.
-*
-* @param string $url The visited URL.
-* @return string The URL to redirect to on login. Must be absolute.
-*/
-function my_forcelogin_redirect( $url ) {
-return home_url( '/' );
-}
-add_filter( 'v_forcelogin_redirect', 'my_forcelogin_redirect' );
+
