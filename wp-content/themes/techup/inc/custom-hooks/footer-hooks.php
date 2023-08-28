5f6f03e3e8cf7e9ca43a25d16557bfe7e57ea265
XAG-Australia <david@thesydney.agency>
2023-08-28 12:36:19 +1000

    Update footer-hooks.php

--- wp-content/themes/techup/inc/custom-hooks/footer-hooks.php
+++ wp-content/themes/techup/inc/custom-hooks/footer-hooks.php
@@ -19,13 +19,13 @@ if( ! function_exists( 'techup_footer_sidebar' ) ):
 	function techup_footer_sidebar(){ ?>
 	    <div class="foot-top" style="height: calc(50vh - 180px); width: 100vw; overflow: hidden; z-index: 9; padding: 0px !important;">
                 
-            <div class="container-left" style="float: left; width: 33.33vw; height: 90%; position: relative; z-index: 9999;">
+            <div class="container-left" style="float: left; width: 33.33vw; height: 90%; position: relative; z-index: 9999; padding: 10px;">
             </div>
-            <div class="container-right" style="float: left; width: 33.33vw; height: 90%; position: relative; z-index: 9999;">
+            <div class="container-right" style="float: left; width: 33.33vw; height: 90%; position: relative; z-index: 9999; padding: 10px;">
             </div>
-			<div class="container-subscribe" style="float: left; width: 33.33vw; height: 90%; position: relative; z-index: 9999;">
+			<div class="container-subscribe" style="float: left; width: 33.33vw; height: 90%; position: relative; z-index: 9999; padding: 10px;">
             </div>
-        <div class="col-sm-12" style="width: 100%; text-align: center; height: 10%;">
+        <div class="col-sm-12" style="width: 100%; text-align: right; height: 10%; font-size: 12px; line-height: 28px; bottom: 0px; float: left; text-transform: uppercase; font-family: 'Roboto Condensed', sans-serif; letter-spacing: 2px; font-weight: 400; text-shadow: none !important;">
                         <?php echo '©' ?> <?php bloginfo( 'name' ); ?> <?php echo '2023' ?>
                     </div>
 	<?php }
