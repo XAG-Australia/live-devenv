c30673e795acfff8d2101308a501c893fb2e9762
XAG-Australia <david@thesydney.agency>
2023-08-28 13:15:01 +1000

    Update footer-hooks.php

--- wp-content/themes/techup/inc/custom-hooks/footer-hooks.php
+++ wp-content/themes/techup/inc/custom-hooks/footer-hooks.php
@@ -21,6 +21,7 @@ if( ! function_exists( 'techup_footer_sidebar' ) ):
 	<link rel="stylesheet" href="https://htmlstream.com/preview/unify-v2.6/assets/css/unify-core.css">
 	<link rel="stylesheet" href="https://htmlstream.com/preview/unify-v2.6/assets/css/unify-components.css">
 	<link rel="stylesheet" href="https://htmlstream.com/preview/unify-v2.6/assets/css/unify-globals.css">
+	<link rel="stylesheet" href="https://htmlstream.com/preview/unify-v2.6/assets/vendor/icon-awesome/css/font-awesome.min.css">
 
 	    <div class="foot-top" style="height: 400px; width: 100vw; overflow: hidden; z-index: 9; padding: 0px !important;">
                 
