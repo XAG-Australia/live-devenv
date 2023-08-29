294e1eee58ef4c8c7f55da80afe7a88a6fb9c620
XAG-Australia <david@thesydney.agency>
2023-08-29 10:33:47 +1000

    Adding css

--- wp-content/themes/techup/header.php
+++ wp-content/themes/techup/header.php
@@ -23,6 +23,7 @@
 	<link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet">
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+	<link href="https://xag-au.com/css/unify-core.css" rel="stylesheet">
 	<?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
